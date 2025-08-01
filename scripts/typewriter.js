e4f134f683b3a1ade726cf92e54ac86367a01a95
Ecyna <159649748+Ecynaa@users.noreply.github.com>
2025-08-01 17:37:42 +0200

    Ajout d'un Typewriter + Fade-In h1

---
+++ scripts/typewriter.js
@@ -0,0 +1,30 @@
+const words = ['/Développeuse', '/Curieuse', '/Créative', '/Passionnée'];
+const typedText = document.getElementById('typed-text');
+const cursor = document.querySelector('.cursor');
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+  const displayedText = currentWord.substring(0, charIndex);
+
+  typedText.textContent = '' + displayedText;
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 50);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) {
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+    setTimeout(typeEffect, 1000);
+  }
+}
+
+window.addEventListener('load', typeEffect);

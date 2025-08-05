4e1f82cc8a26741e5a8e60bb04b87fbff20d7466
Ecyna <159649748+Ecynaa@users.noreply.github.com>
2025-08-05 18:08:20 +0200

    Add Custom_Cursor

---
+++ scripts/custom_cursor.js
@@ -0,0 +1,59 @@
+const dot = document.querySelector('.cursor-dot');
+  const outline = document.querySelector('.cursor-outline');
+  const bg = document.querySelector('.cursor-bg');
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let outlineX = 0;
+  let outlineY = 0;
+
+  const lerp = (start, end, factor) => (1 - factor) * start + factor * end;
+
+  function animateCursor() {
+    outlineX = lerp(outlineX, mouseX, 0.1);
+    outlineY = lerp(outlineY, mouseY, 0.1);
+
+    dot.style.left = `${mouseX}px`;
+    dot.style.top = `${mouseY}px`;
+
+    outline.style.left = `${outlineX}px`;
+    outline.style.top = `${outlineY}px`;
+
+    bg.style.left = `${mouseX}px`;
+    bg.style.top = `${mouseY}px`;
+
+    requestAnimationFrame(animateCursor);
+  }
+
+  document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  document.addEventListener('mousedown', () => {
+    bg.style.width = '30px'; // correspond à la taille du cercle
+    bg.style.height = '30px';
+    bg.style.opacity = '0.5';
+  });
+
+  document.addEventListener('mouseup', () => {
+    bg.style.width = '0px'; // retour à moitié
+    bg.style.height = '0px';
+    bg.style.opacity = '0.1';
+  });
+
+  document.querySelectorAll('a, button').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+      bg.style.width = '15px';
+      bg.style.height = '15px';
+      bg.style.opacity = '0.5';
+    });
+
+    el.addEventListener('mouseleave', () => {
+      bg.style.width = '0px';
+      bg.style.height = '0px';
+      bg.style.opacity = '0';
+    });
+  });
+
+  animateCursor();
